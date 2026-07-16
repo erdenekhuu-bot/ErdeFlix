342ca10bb35978ba48e96541123f70cdc4ded62d
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-07-16 14:43:20 +0800

    .

--- deployment-action.sh
+++ deployment-action.sh
@@ -45,3 +45,6 @@ find bootstrap/cache -type d -exec chmod 775 {} \;
 find bootstrap/cache -type f -exec chmod 664 {} \;
 
 echo "===== Deployment completed ====="
+
+
+#pm2 start cloudflared --name "erdeflix-tunnel" -- tunnel --url http://192.168.1.50:8000
