74aa28e0c7996a49743e7013badf9e1b08233155
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-07-27 15:19:02 +0800

    'view'

--- app/Http/Controllers/Dashboard/IndexController.php
+++ app/Http/Controllers/Dashboard/IndexController.php
@@ -6,10 +6,15 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use Laravel\Fortify\Http\Requests\LoginRequest;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+use Laravel\Fortify\Contracts\LoginResponse;
+use Illuminate\Contracts\Auth\StatefulGuard;
 
 class IndexController extends Controller
 {
-    public function info():Response {
+    public function info(): Response
+    {
         return Inertia::render('dashboard/Dashboard');
     }
 }
