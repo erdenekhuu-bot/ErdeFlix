050b0bc31821518142b0726aa2ec3ac8dcc12d9f
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-08-03 09:35:39 +0800

    customized.new

--- app/Models/Reaction.php
+++ app/Models/Reaction.php
@@ -3,7 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Table;
 
+#[Table('reactions', key: 'id')]
+#[Fillable(['movie_id', 'user_id', 'type', 'like', 'dislike', 'comment'])]
 class Reaction extends Model
 {
     //
